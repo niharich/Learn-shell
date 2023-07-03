4394509a82c6c9ffe95cb100d58dcea883c475e0
Nihari <nxc91400@ucmo.edu>
2023-07-03 12:47:21 -0500

    update

--- sample1.sh
+++ sample1.sh
@@ -1,1 +1,6 @@
 echo Niharika
+echo -e "\e[31mNiharika in Red\e[0m"
+echo -e "\e[32mNiharika in Green\e[0m"
+echo -e "\e[33mNiharika in Yellow\e[0m"
+echo -e "\e[34mNiharika in Blue\e[0m"
+echo -e "\e[35mNiharika in Magenta\e[0m"
